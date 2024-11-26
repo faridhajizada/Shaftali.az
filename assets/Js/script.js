fdbe57093e77405be1e80fe290bf7fe0282e8ad0
Hajizada Farid <faridmh@code.edu.az>
2024-11-26 16:56:10 +0400

    Delete unused image files and update mobile menu styles

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -3,15 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector("main").style.marginTop = headerHeight + "px";
 });
 
-console.log("Hello World");
-
-var swiper = new Swiper(".swiper-container", {
+var swiper = new Swiper("#stats-carausel", {
   slidesPerView: 5,
   spaceBetween: 10,
   loop: true,
   autoplay: {
-    delay: 2000, // Интервал между слайдами в миллисекундах
-    disableOnInteraction: false, // Автоплей не останавливается при взаимодействии
+    delay: 2000,
+    disableOnInteraction: false,
   },
 
   breakpoints: {
@@ -34,65 +32,60 @@ var swiper = new Swiper(".swiper-container", {
   },
 });
 
+var swiper = new Swiper("#team-carousel", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 2,
+      spaceBetween: 5,
+    },
+    "@0.75": {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    "@1.50": {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+  },
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   new Swiper(".blog-swiper", {
     loop: false,
     autoplay: {
-      delay: 2500, // Задержка между слайдами
+      delay: 2000,
       disableOnInteraction: false,
     },
-    effect: "slide", // Плавное движение слайдов
-    speed: 600, // Устанавливает плавность перехода (600ms)
-    slidesPerView: 1, // Количество слайдов, отображаемых одновременно
-    spaceBetween: 10, // Расстояние между слайдами
+    effect: "slide",
+    speed: 600,
+    slidesPerView: 1,
+    spaceBetween: 10,
   });
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBurger = document.querySelector(".icon-menu");
+  const mobileMenu = document.getElementById("mobile-menu");
 
-
-
-
-// -------------------  Contact Form  -------------------
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const radioButtons = document.querySelectorAll('input[name="service"]');
-//   const serviceOptions = document.querySelector('.options');
-//   const contactForm = document.getElementById("contact-form");
-//   const submitBtn = document.getElementById("submit-btn");
-
-//   radioButtons.forEach(button => {
-//     button.addEventListener("change", function () {
-//       // Скрываем выбор услуги плавно
-//       serviceOptions.style.opacity = 0;
-//       setTimeout(function () {
-//         serviceOptions.style.display = "none";  // Убираем с экрана после анимации
-//         // Показываем форму
-//         contactForm.style.display = "block";
-//         setTimeout(function () {
-//           contactForm.style.opacity = 1;  // Плавно показываем форму
-//         }, 50);
-//       }, 500);  // Время для плавного исчезновения блока
-//     });
-//   });
-
-//   submitBtn.addEventListener("click", function () {
-//     const name = document.getElementById("name").value;
-//     const phone = document.getElementById("phone").value;
-//     const selectedService = document.querySelector('input[name="service"]:checked');
-
-//     if (name && phone && selectedService) {
-//       alert(`Xidmət: ${selectedService.value}\nAd: ${name}\nTelefon: ${phone}`);
-//       // Логика отправки данных на сервер
-
-//       // Очистка формы
-//       document.getElementById("name").value = "";
-//       document.getElementById("phone").value = "";
-//       contactForm.style.display = "none";  // Скрываем форму
-//       serviceOptions.style.display = "flex";  // Показываем блок с вариантами снова
-//       serviceOptions.style.opacity = 1;  // Плавно показываем блок с услугами
-//     } else {
-//       alert("Zəhmət olmasa xidmət seçin və məlumatları daxil edin.");
-//     }
-//   });
-// });
+  menuBurger.addEventListener("click", () => {
+    if (mobileMenu.classList.contains("hidden")) {
+      mobileMenu.classList.remove("hidden");
+      mobileMenu.classList.add("visible");
+    } else {
+      mobileMenu.classList.remove("visible");
+      mobileMenu.classList.add("hidden");
+    }
+  });
+});
